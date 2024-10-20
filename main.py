279b3977b4c2dcdd24e588180bf0a6e3eb6413d7
Huk <minimickey427@yahoo.com>
2024-10-20 14:35:05 +0300

    redirect video

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import time
 
 from selenium import webdriver
-
 driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get('https://momazosdiego.com/momazo.mp4')
-time.sleep(500000)
+def momazosdiego():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('file:///Users/scarlatdorianandrei/PycharmProjects/sigma/diego.mp4')
+    time.sleep(13)
+    driver.quit()
+
+def momazospablo():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("file:///Users/scarlatdorianandrei/PycharmProjects/sigma/momazos%20pablo.mp4")
+    driver.maximize_window()
+    time.sleep(6.5)
+    driver.quit()
+
+def momazosluis():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("file:///Users/scarlatdorianandrei/PycharmProjects/sigma/Momazos%20Luis.mp4")
+    time.sleep(7)
+    driver.quit()
+
+def momazosjuan():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("file:///Users/scarlatdorianandrei/PycharmProjects/sigma/momazos%20Jua.mp4")
+    time.sleep(15)
+    driver.quit()
+
+
+
+momazosdiego()
+momazospablo()
+momazosluis()
+momazosjuan()
